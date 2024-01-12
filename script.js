c906c25f4a303d58fc166e1d753cdba30e9bd36c
Samadhan Gaikwad <samadhangaikwad9049@gmail.com>
2024-01-12 20:12:39 +0530

    Created Dom project using DOM manipulation

---
+++ script.js
@@ -0,0 +1,10 @@
+let div = document.getElementById("demo");
+
+div.style.backgroundColor = "yellow"
+div.style.margin = "20px";
+div.style.padding = "10px"
+div.style.fontSize = "20px";
+div.style.fontWeight = "bold"
+div.style.height = "200px";
+div.style.width = "300px";
+
